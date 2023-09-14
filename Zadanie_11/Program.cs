1619caaa143fb7cd1ee09d130e571ea4af50bb95
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-14 22:09:07 +0300

    Zadanie_11

---
+++ Zadanie_11/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+//7->28
+//4->10
+//8->36 
+
+int GetNumber(string message)
+{
+    System.Console.Write($"Введите число {message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+//Опишем метод, который будет суммировать элементы
+int GetSum(int num)
+{
+    int sum=0;
+    for(int i=1; i<=num; i++)
+    {
+        sum += i;
+    }
+    return(sum);
+}
+
+int numA = GetNumber("A = ");
+int sum = GetSum(numA);
+Console.WriteLine($"sum = {sum}");
