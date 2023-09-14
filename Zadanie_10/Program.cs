49d56205be0f8424066c04a670d083f089c2fa23
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-14 11:39:00 +0300

    Zadanie10

---
+++ Zadanie_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N. 
+//5=> 1, 4, 9, 16, 25
+//2=> 1, 4
+
+int GetNumber(string message)
+{
+    System.Console.Write($"Введите число {message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int num01 = GetNumber(" = "); 
+int num02 = 1; 
+
+while(num02<=num01)
+{
+    System.Console.Write($"{num02*num02}, ");
+    num02++;
+}
