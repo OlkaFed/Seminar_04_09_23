a343ab708ab553498bc8cc89936f9836b4d55663
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-14 23:02:40 +0300

    Zadanie_12

---
+++ Zadanie_12/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+//456->3
+//78->2
+//89126->5
+
+int GetNumber(string message)
+{
+    System.Console.Write($"Введите число {message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int GetCount(int num)
+{
+    int count = 0;
+    for(; 0<num; num/=10)
+    {
+        count = count+1; 
+    }
+    return count;
+}
+
+int num = GetNumber("A ");
+int result = GetCount(num); 
+Console.WriteLine($"В числе {result} цифр");
