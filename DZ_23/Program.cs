faec125bec901d15e6aee80265e6ec83c6557da9
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-14 13:27:06 +0300

    dz_23

---
+++ DZ_23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int GetNumber(string message)
+{
+    System.Console.Write($"Введите число {message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int num01 = GetNumber(" = "); 
+int num02 = 1; 
+
+while(num02<=num01)
+{
+    System.Console.Write($"{num02*num02*num02}, ");
+    num02++;
+}
