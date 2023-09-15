ab624e141a7600d07babfc02b46bec601d43f499
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-15 10:42:44 +0300

    DZ_25

---
+++ DZ_25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int GetNumber(string message)
+{
+    System.Console.Write($"Введите число {message} : ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int numA = GetNumber("A = ");
+int numB = GetNumber("B = "); 
+
+double sqr = Math.Pow(numA, numB); //Возведение в степень 
+
+Console.WriteLine($"Результат возведения числа А в степень В = {sqr}");
