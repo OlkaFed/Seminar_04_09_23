4f16e90652721b67483a89f2a824eeb61967cf7f
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-18 21:33:23 +0300

    Zadanie_21

---
+++ Zadanie_21/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10*/
+
+int take()
+{
+                  Console.WriteLine("Enter number");
+                  int number = Convert.ToInt32(Console.ReadLine());
+                  return number;
+}
+int[] Convertation(int number)
+{
+                  int[] array = new int[32];
+                  for (int i = 0; number > 0; i++)
+                  {
+                                    array[array.Length - 1 - i] = number % 2;
+                                    number /= 2;
+                  }
+                  return array;
+}
+Console.WriteLine();
+int number = take();
+string print = string.Join(", ", Convertation(number));
+Console.WriteLine(print);
