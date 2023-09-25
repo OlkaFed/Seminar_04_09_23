33e3de1e0f1fb48628987a1dc33d0a96432876e8
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-25 20:48:08 +0300

    Zadanie_27

---
+++ Zadanie_27/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Задача 53: Задайте двумерный массив. 
+Напишите программу, которая поменяет местами первую и последнюю строку массива.*/
+
+int[,] GetMatrix(int rows, int columns, int min = 0, int max = 9)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.BackgroundColor = ConsoleColor.Black;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) Console.ForegroundColor = ConsoleColor.Green;
+            else Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SetNumber(string message)
+{
+    Console.Write($"Введите число {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void Change(int[,] matrix)
+{
+    int temp;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0,i];
+        matrix[0,i] = matrix[matrix.GetLength(0)-1,i];
+        matrix[matrix.GetLength(0)-1,i] = temp;
+    }
+}
+
+int rows = SetNumber("- количество строк");
+int columns = SetNumber("- количество столбцов");
+
+
+int[,] result = GetMatrix(rows, columns);
+PrintMatrix(result);
+Console.WriteLine();
+Change(result); 
+PrintMatrix(result);
