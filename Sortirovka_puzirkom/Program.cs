cf8c82e0a91dbf67f8955696e7d532ff3eee5d6a
Olya <Olya.fedorowskaya@yandex.ru>
2023-10-05 23:44:54 +0300

    Sortirovka_puzirkom

---
+++ Sortirovka_puzirkom/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Cортировка пузырьком */
+int n = 10; 
+int max = 100; 
+int[] array = new int[n]; 
+for (int i = 0; i < n; i++)
+{
+    array[i] = Random.Shared.Next(max);
+}
+Console.WriteLine($"[{String.Join(',', array)}]");
+
+for (int k = 0; k < n-1; k++)
+//{
+    for (int i = 0; i < n-1; i++)
+    {
+        if (array[i]>array[i+1])
+        {
+            int temp = array[i]; 
+            array[i] = array[i+1];
+            array[i+1] = temp;
+        }
+    }
+//}
+Console.WriteLine($"[{String.Join(',', array)}]");
