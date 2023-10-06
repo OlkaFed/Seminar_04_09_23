41544efa645cbfcdc33c3948fce58b528044e271
Olya <Olya.fedorowskaya@yandex.ru>
2023-10-06 08:26:40 +0300

    Sortirovka_puzirkom_pravki

--- Sortirovka_puzirkom/Program.cs
+++ Sortirovka_puzirkom/Program.cs
@@ -2,15 +2,26 @@
 int n = 10; 
 int max = 100; 
 int[] array = new int[n]; 
+
+bool Check(int[] array) 
+{
+    int size = array.Length; 
+    for (int i = 0; i < size-1; i++)
+    {
+        if(array[i]>array[i+1]) return false; 
+    }
+    return true; 
+}
+
 for (int i = 0; i < n; i++)
 {
     array[i] = Random.Shared.Next(max);
 }
 Console.WriteLine($"[{String.Join(',', array)}]");
-
+Console.WriteLine(Check(array));
 for (int k = 0; k < n-1; k++)
-//{
-    for (int i = 0; i < n-1; i++)
+{
+    for (int i = 0; i < n-1-k; i++)
     {
         if (array[i]>array[i+1])
         {
@@ -19,5 +30,9 @@ for (int k = 0; k < n-1; k++)
             array[i+1] = temp;
         }
     }
-//}
+}
 Console.WriteLine($"[{String.Join(',', array)}]");
+Console.WriteLine(Check(array));
+
+//int j = 0; //вариант решения более простой 
+//while (j<n) Console.Write(array[j++] + " ");
